d8079dc270c742d98f4e28d4c3ff5921bbec9872
Matt Ware <mattbware3@gmail.com>
2024-09-18 16:25:48 +0100

    Updates

---
+++ js/home.js
@@ -0,0 +1,1 @@
+(()=>{let e="ttwa.re";window.addEventListener("load",(function(){let t=document.getElementById("email-box"),n=document.getElementById("email-button");t.value=`m@${e}`,n.href=`mailto:m@${e}`}))})();
