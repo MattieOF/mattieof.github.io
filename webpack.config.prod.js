cc36de31636cf78f8050872b6a10b100a2b9fdb9
Matt Ware <mattbware3@gmail.com>
2024-09-19 01:33:19 +0100

    Fix build (include blog and contact pages)
    Also minimize all html

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
   mode: 'production',
@@ -20,8 +21,17 @@ module.exports = merge(common, {
         { from: 'robots.txt', to: 'robots.txt' },
         { from: 'icon.png', to: 'icon.png' },
         { from: '404.html', to: '404.html' },
+        { from: 'blog.html', to: 'blog.html' },
+        { from: 'contact.html', to: 'contact.html' },
         { from: 'site.webmanifest', to: 'site.webmanifest' },
       ],
     }),
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      `...`,
+      new HtmlMinimizerPlugin(),
+    ]
+  },
 });
