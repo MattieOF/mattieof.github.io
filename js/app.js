6f2256910b6bf829e564905da3e3358c0edb5e21
Matt Ware <mattbware3@gmail.com>
2024-09-19 01:25:56 +0100

    Improve theme handling

--- js/app.js
+++ js/app.js
@@ -1,22 +1,33 @@
 import '../css/style.css';
 
-if (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  localStorage.theme = 'dark';
-}
-
 function applyTheme() {
-  document.documentElement.classList.toggle('dark', localStorage.theme === 'dark');
+  // Figure out if the theme should be dark
+  let dark = false;
+  if ('theme' in localStorage) {
+    dark = localStorage.theme === 'dark';
+  } else {
+    dark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  }
+
+  // Update the class list to apply the theme
+  document.documentElement.classList.toggle('dark', dark);
+
+  // Update the theme toggler button (if it has been created) to reflect the current theme
   const button = document.getElementsByName("theme-button");
   if (button.length > 0) { // The button won't exist when this is first called
     button[0].innerText = localStorage.theme === 'dark' ? '☀️' : '🌙';
   }
 }
+
+// Apply the theme on page load
 applyTheme();
 
 window.toggleDarkMode = function() {
   if ('theme' in localStorage) {
     localStorage.theme = localStorage.theme === 'dark' ? 'light' : 'dark';
   } else {
+    // If the user has manually toggled the theme, we should respect that over the system preference going forward,
+    // so we add the theme to localStorage
     localStorage.theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'light' : 'dark';
   }
   applyTheme();
