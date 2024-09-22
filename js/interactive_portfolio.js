1c52a3325da0577d22261e2162bbd17bf3e440d3
Matt Ware <mattbware3@gmail.com>
2024-09-22 18:10:21 +0100

    Test model

--- js/interactive_portfolio.js
+++ js/interactive_portfolio.js
@@ -5,16 +5,13 @@ let canvas = document.getElementById("interactive-canvas");
 
 const clock = new THREE.Clock();
 const scene = new THREE.Scene();
+scene.add(new THREE.DirectionalLight(0xBBCCFF, 3));
+scene.add(new THREE.AmbientLight(0x606060));
 const camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 renderer.setSize(document.body.clientWidth, window.innerHeight);
 renderer.setClearColor(new THREE.Color(0x6495ED), 1);
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
 camera.position.z = 5;
 
@@ -24,13 +21,35 @@ window.onresize = ev => {
   camera.updateProjectionMatrix();
 }
 
+let objects = []
+
+void loadData();
 animate();
+
+async function loadData() {
+  require.ensure(['three/examples/jsm/loaders/GLTFLoader'], function(require) {
+    let GLTFLoader = require('three/examples/jsm/loaders/GLTFLoader').GLTFLoader;
+
+    const loader = new GLTFLoader();
+    loader.load('PortfolioAssets/PlanetTest.glb', function(gltf) {
+      let planet = gltf.scene.children[0];
+      planet.material = new THREE.MeshPhysicalMaterial({ color: new THREE.Color(.3, .3, .3), flatShading: true });
+      console.log(planet.material);
+      scene.add(planet);
+      objects.push(planet);
+    });
+  });
+}
+
 function animate() {
   requestAnimationFrame(animate);
 
   const delta = clock.getDelta();
 
-  cube.rotation.x += 10 * delta;
-  cube.rotation.y += 10 * delta;
+  for (const object of objects) {
+    object.rotation.x += 2 * delta;
+    object.rotation.y += 2 * delta;
+  }
+
   renderer.render(scene, camera);
 }
