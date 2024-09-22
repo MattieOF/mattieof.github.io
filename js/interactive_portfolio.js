cbee6aae66ab1b50880c10452a2f6ec657815cc3
Matt Ware <mattbware3@gmail.com>
2024-09-22 19:39:22 +0100

    Fade out bottom of canvas

--- js/interactive_portfolio.js
+++ js/interactive_portfolio.js
@@ -2,12 +2,14 @@
 
 let THREE = require('three');
 let canvas = document.getElementById("interactive-canvas");
+let mouseNDC = new THREE.Vector2();
 
 const clock = new THREE.Clock();
 const scene = new THREE.Scene();
 scene.add(new THREE.DirectionalLight(0xBBCCFF, 3));
 scene.add(new THREE.AmbientLight(0x606060));
 const camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
+let raycaster = new THREE.Raycaster(camera.position, new THREE.Vector3(0, 0, -1));
 
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 renderer.setSize(document.body.clientWidth, window.innerHeight);
@@ -21,23 +23,30 @@ window.onresize = ev => {
   camera.updateProjectionMatrix();
 }
 
-let objects = []
+let planets = []
 
 void loadData();
 animate();
 
 async function loadData() {
-  require.ensure(['three/examples/jsm/loaders/GLTFLoader'], function(require) {
+  await require.ensure(['three/examples/jsm/loaders/GLTFLoader'], async function (require) {
     let GLTFLoader = require('three/examples/jsm/loaders/GLTFLoader').GLTFLoader;
 
     const loader = new GLTFLoader();
-    loader.load('PortfolioAssets/PlanetTest.glb', function(gltf) {
-      let planet = gltf.scene.children[0];
-      planet.material = new THREE.MeshPhysicalMaterial({ color: new THREE.Color(.3, .3, .3), flatShading: true });
-      console.log(planet.material);
-      scene.add(planet);
-      objects.push(planet);
-    });
+    const planetScene = await loader.loadAsync('PortfolioAssets/PlanetTest.glb');
+
+    let planet = planetScene.scene.children[0];
+    planet.material = new THREE.MeshPhysicalMaterial({color: new THREE.Color(.3, .3, .3), flatShading: true});
+
+    for (let i = 0; i < 10; i++) {
+      let newPlanet = planet.clone();
+      newPlanet.position.x = randomBetween(-20, 20);
+      newPlanet.position.y = randomBetween(-20, 20);
+      newPlanet.position.z = randomBetween(-20, -10);
+      newPlanet.scale.setScalar(randomBetween(0.1, 2));
+      planets.push(newPlanet);
+      scene.add(newPlanet);
+    }
   });
 }
 
@@ -46,10 +55,17 @@ function animate() {
 
   const delta = clock.getDelta();
 
-  for (const object of objects) {
+  // Get NDC mouse coords
+  raycaster.setFromCamera(mouseNDC, camera);
+
+  for (const object of planets) {
     object.rotation.x += 2 * delta;
     object.rotation.y += 2 * delta;
   }
 
   renderer.render(scene, camera);
+}
+
+function randomBetween(min, max) {
+  return Math.random() * (max - min) + min;
 }
