c6a6962b12deb0e9aa00ff25b372a9e73bd3bcdd
Matt Ware <mattbware3@gmail.com>
2024-12-01 20:13:46 +0000

    Portfolio improvements
    Made the portfolio significantly more responsive
    Had to sacrifice unfolding anim tho :(
    Added posters for the videos as they load
    Made /cv redirect to the pdf, instead of showing the HTML version

--- js/home.js
+++ js/home.js
@@ -28,20 +28,37 @@ window.addEventListener('load', function() {
     observer.observe(video);
   }
 
-  let projects = document.getElementsByClassName("portfolio-project");
-  for (let i = 0; i < projects.length; i++) {
-    let proj = projects[i];
-    proj.style.height = `${proj.scrollHeight}px`;
-    proj.setAttribute("initialHeight", proj.scrollHeight.toString()); // Cache the initial height for a hack later (see portfolioItemClicked)
-  }
+  // let projects = document.getElementsByClassName("portfolio-project");
+  // for (let i = 0; i < projects.length; i++) {
+  //   let proj = projects[i];
+  //   proj.style.height = `${proj.scrollHeight}px`;
+  // }
 });
 
 window.portfolioItemClicked = function(item) {
   // MW @hack: For some reason, when hiding the long description, the height is not reset to the initial height
   // (and is usually just ~4px shorter than with it visible).
   // So, we cache the initial height, and then set it back to it if we've just hidden the long description.
-  document.getElementById(item + "-desc").classList.toggle("hidden");
-  var longDescVisible = !document.getElementById(item + "-more-desc").classList.toggle("hidden");
-  let proj = document.getElementById("proj-" + item);
-  proj.style.height = longDescVisible ? `${proj.scrollHeight}px` : `${proj.getAttribute("initialHeight")}px`;
+
+  const desc = document.getElementById(item + "-desc");
+  const longDesc = document.getElementById(item + "-more-desc");
+
+  desc.classList.toggle("hidden");
+  longDesc.classList.toggle("hidden");
+
+  // if (desc.style.display == "none") {
+  //   desc.style.display = "block";
+  //   longDesc.style.display = "none";
+  // } else {
+  //   desc.style.display = "none";
+  //   longDesc.style.display = "block";
+  // }
+
+  // let proj = document.getElementById("proj-" + item);
+  // proj.style.height = `${proj.scrollHeight}px`;
+
+  // document.getElementById(item + "-desc").classList.toggle("hidden");
+  // var longDescVisible = !document.getElementById(item + "-more-desc").classList.toggle("hidden");
+  // let proj = document.getElementById("proj-" + item);
+  // proj.style.height = longDescVisible ? `${proj.scrollHeight}px` : `${proj.getAttribute("initialHeight")}px`;
 }
